d05e7b7863b6d46ad2a408877882dbccc25b7a4c
SeptianTito123 <SeptianTito123>
2023-10-16 17:10:42 +0700

    pengembalian

--- Perpustakaan - Copy.java
+++ Perpustakaan - Copy.java
@@ -53,28 +53,45 @@ public class Perpustakaan {
                                 break;
 
                             case '2':
-                                System.out.print("Masukkan nama peminjam               : ");
-                                nama = input3.nextLine();
-                                System.out.print("Masukkan nim peminjam                : ");
-                                nim = input3.nextLine();
-                                System.out.print("Masukkan judul buku yang dipinjam    : ");
-                                judul = input3.nextLine();
-                                System.out.print("Masukkan jumlah buku yang dipinjam   : ");
-                                jumlah = input3.nextInt();
-                                System.out.print("Masukkan jumlah hari peminjaman      : ");
-                                hari = input3.nextInt();
-                                telat = hari - 7;
-                                if (hari > 7) {
+                                int jumlah, telat, hari=0;
+                                float nim;
+                                double denda, tenggat;
+                                String konfirmasi,judul,nama;
+                                    
+                                do {
+                                    System.out.print("Apakah anda ingin mengembalikan buku (y/t)? ");
+                                    konfirmasi = input2.next();
 
-                                    double denda;
-                                    denda = (hari - 7) * 5000;
-                                    System.out.println("Peminjam " + nama + " terkena denda sebesar " + denda
-                                            + " karena terlambat mengembalikan" + telat + " hari");
-                                } else {
-                                    System.out.println("Buku berhasil dikembalikan");
-                                }
-                                break;
-                            case '3':
+                                    if (konfirmasi.equalsIgnoreCase("y")) {
+                                        System.out.print("Masukkan nama peminjam               : ");
+                                        nama = input3.nextLine();
+                                        System.out.print("Masukkan nim peminjam                : ");
+                                        nim = input2.nextFloat();
+                                        System.out.print("Masukkan judul buku yang dipinjam    : ");
+                                        judul = input3.nextLine();
+                                        System.out.print("Masukkan jumlah buku yang dipinjam   : ");
+                                        jumlah = input2.nextInt();
+                                        System.out.print("Masukkan jumlah hari peminjaman      : ");
+                                        hari = input2.nextInt();
+                                        
+                                        telat = hari - 7;
+                                        tenggat = 7; 
+
+                                        if (hari > tenggat) {   
+                                        denda =(hari - 7) * 5000;
+                                        System.out.println("Peminjam " + nama + " terkena denda sebesar " + denda + " karena terlambat mengembalikan buku " + telat + " hari. ");
+
+                                        } else {
+                                        System.out.println("Buku berhasil dikembalikan");
+
+                                        } 
+                                        }if (konfirmasi.equalsIgnoreCase("t")) {
+                                            break;
+                                        }
+                                } while (hari > 7);
+                            }
+                        }                            
+                        case '3':
                                 int stokawal, pinjam, hilang, rusak, stokakhir;
                                 System.out.print("Masukkan Stok buku awal               : ");
                                 stokawal = input2.nextInt();
