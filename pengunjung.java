bf5a95a55079ae4265aa52925b2a8fd0d29ba7a5
DapedGuardiola <muhammaddavid81@gmail.com>
2023-10-29 23:31:16 +0700

    update daped

--- pengunjung.java
+++ pengunjung.java
@@ -45,21 +45,12 @@ public class pengunjung {
                                 String[] terbit ={"2002", "2003", "2005"};
                                 boolean tersedia = false;
                                 for (int j=0; j < judul.length; j++) {
-<<<<<<< HEAD
                                     if (jdlBuku.equalsIgnoreCase(judul[j])) {
-=======
-                                    if (judul[j].equalsIgnoreCase(jdlBuku)) {
-                                        tersedia = true;
->>>>>>> 8be6261f3fa07a46429acf2e6dc36262417cd471
                                         System.out.println("=== Buku Tersedia ===");
                                         System.out.println("Judul : "+judul[j]);
                                         System.out.println("Penulis : "+penulis[j]);
                                         System.out.println("Tahun Terbit : "+terbit[j]);
-<<<<<<< HEAD
                                         tersedia = false;
-=======
-                                        
->>>>>>> 8be6261f3fa07a46429acf2e6dc36262417cd471
                                         break;
                                     } if (!tersedia){
                                         System.out.println("Buku tidak tersedia");
@@ -68,7 +59,6 @@ public class pengunjung {
                                 
                                 System.out.println("Apakah nda ingin melakukan transaksi lain (Ya / Tidak)");
                                 pilihan1 = scan1.nextLine();
-                    
                         } 
                             else {
                                 System.out.println("Anda tidak memiliki wewenang untuk mengakses laman lain");
